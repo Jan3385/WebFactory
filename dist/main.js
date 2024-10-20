27f20720bbf35695663da5f434360819de633e95
Hat Dealer <jan.richter2007@gmail.com>
2024-10-20 21:32:39 +0200

    forgot to compile

--- dist/main.js
+++ dist/main.js
@@ -680,7 +680,8 @@ async function Main() {
         RenderManager.ins.Draw(); // draws everything
         let endTime = performance.now();
         const executionTime = endTime - startTime;
-        //console.log((1/fps*1000) - executionTime);
+        if (executionTime > 16)
+            console.log("Lag spike!", (1 / fps * 1000) - executionTime);
         await new Promise(r => setTimeout(r, Math.max((1 / fps * 1000) - executionTime, 0)));
     }
 }
